5b7591c768ff6dfce4e66d6c55ed9697afafc062
Malondonof <marktigrelon@gmail.com>
2024-11-22 20:17:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import streamlit as st
 import base64
 from openai import OpenAI
 import openai
-from gtts import gTTS
 from PIL import Image
 
 # Function to encode the image to base64
@@ -11,14 +10,18 @@ def encode_image(image_file):
     return base64.b64encode(image_file.getvalue()).decode("utf-8")
 
 
-st.set_page_config(page_title="Analisis de imagen", layout="centered", initial_sidebar_state="collapsed")
+st.set_page_config(page_title="Analisis dde imagen", layout="centered", initial_sidebar_state="collapsed")
 # Streamlit page setup
 st.title("LectorManga")
+#image = Image.open('OIG4.jpg')
+#st.image(image, width=350)
 with st.sidebar:
-    st.subheader("¡Hola! En esta app podrás obtener descripciones detalladas de la página manga que requieras.")
+    st.subheader("¡Hola! En está app podrás obtener descripciones detalladas de la pagina manga que requieras.")
 
-ke = st.text_input('Ingresa tu Clave', type="password")
+ke = st.text_input('Ingresa tu Clave')
+#os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
 os.environ['OPENAI_API_KEY'] = ke
+
 
 # Retrieve the OpenAI API Key from secrets
 api_key = os.environ['OPENAI_API_KEY']
@@ -27,74 +30,93 @@ api_key = os.environ['OPENAI_API_KEY']
 client = OpenAI(api_key=api_key)
 
 # File uploader allows user to add their own image
-uploaded_file = st.file_uploader("Sube una imagen", type=["jpg", "png", "jpeg"])
+uploaded_file = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
 
 if uploaded_file:
     # Display the uploaded image
-    with st.expander("Imagen subida", expanded=True):
+    with st.expander("Image", expanded = True):
         st.image(uploaded_file, caption=uploaded_file.name, use_column_width=True)
 
 # Toggle for showing additional details input
-show_details = st.checkbox("Añadir detalles sobre la imagen", value=False)
+show_details = st.toggle("Adiciona detalles sobre la imagen", value=False)
 
 if show_details:
     # Text input for additional details about the image, shown only if toggle is True
     additional_details = st.text_area(
-        "Añade contexto de la imagen aquí:",
+        "Adiciona contexto de la imagen aqui:",
         disabled=not show_details
     )
 
 # Button to trigger the analysis
-analyze_button = st.button("Analizar la imagen")
+analyze_button = st.button("Analiza la imagen", type="secondary")
 
 # Check if an image has been uploaded, if the API key is available, and if the button has been pressed
 if uploaded_file is not None and api_key and analyze_button:
 
-    with st.spinner("Analizando..."):
+    with st.spinner("Analizando ..."):
         # Encode the image
         base64_image = encode_image(uploaded_file)
     
         # Optimized prompt for additional clarity and detail
-        prompt_text = (
-            "Eres un lector de manga, un estilo de cómic que se lee de derecha a izquierda. "
-            "Estás aquí para asistir a los usuarios que tengan dificultades para interpretar imágenes o cómics. "
-            "Analiza cada panel y proporciona una descripción detallada. "
-            "Ejemplo: - Mira, Pablo - dice Juan mientras observa intensamente a Pablo, quien responde - No hay nada que puedas hacer. -"
-        )
+        prompt_text = ("Eres un lector de manga, un estilo de comic que se lee de derecha a izquierda. Estas para asistir a los usuarios que tengan dificultades para interpretar imagenes o comic. Analizaras cada anel y le daras una descripcion detallada al usuario. Ejemplo , en este panel se ve a un personaje con gafas y dice-texto- mientras ve a otro personaje . Separa sus dialogos en lineas organizadas comosi fuera un guion. ejemplo 2, -Mira pablo -dice juan mientras ve a pablo la admosfera se intensifica y pablo dice-no hay nada que puedas hacer-"  )
+
+        prompt_text = ("Describe what you see in the image in spanish")
     
         if show_details and additional_details:
             prompt_text += (
-                f"\n\nContexto adicional proporcionado por el usuario:\n{additional_details}"
+                f"\n\nAdditional Context Provided by the User:\n{additional_details}"
             )
+    
+        # Create the payload for the completion request
+        messages = [
+            {
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": prompt_text},
+                    {
+                        "type": "image_url",
+                        "image_url": f"data:image/jpeg;base64,{base64_image}",
+                    },
+                ],
+            }
+        ]
     
         # Make the request to the OpenAI API
         try:
-            response = openai.ChatCompletion.create(
-                model="gpt-4",
-                messages=[{"role": "user", "content": prompt_text}],
-                max_tokens=300,
-            )
-            
-            # Get the generated text
-            analysis_result = response.choices[0].message["content"]
-            st.markdown("### Resultado del análisis:")
-            st.write(analysis_result)
-            
-            # Convert the result to audio
-            st.markdown("### Escuchar resultado:")
-            tts = gTTS(text=analysis_result, lang="es")
-            audio_file = "output.mp3"
-            tts.save(audio_file)
-            
-            # Play the audio
-            audio_bytes = open(audio_file, "rb").read()
-            st.audio(audio_bytes, format="audio/mp3")
-            
+            full_response = ""
+            message_placeholder = st.empty()
+            response = openai.chat.completions.create(
+              model= "gpt-4o-mini",
+              messages=[
+                {
+                   "role": "user",
+                   "content": [
+                     {"type": "text", "text": prompt_text},
+                     {
+                       "type": "image_url",
+                       "image_url": {
+                         "url": f"data:image/jpeg;base64,{base64_image}",
+                       },
+                     },
+                   ],
+                  }
+                ],
+              max_tokens=300,
+              )
+            #response.choices[0].message.content
+            if response.choices[0].message.content is not None:
+                    full_response += response.choices[0].message.content
+                    message_placeholder.markdown(full_response + "▌")
+            # Final update to placeholder after the stream ends
+            message_placeholder.markdown(full_response)
+    
+            # Display the response in the app
+            #st.write(response.choices[0])
         except Exception as e:
-            st.error(f"Ocurrió un error: {e}")
+            st.error(f"An error occurred: {e}")
 else:
     # Warnings for user action required
     if not uploaded_file and analyze_button:
-        st.warning("Por favor, sube una imagen.")
+        st.warning("Please upload an image.")
     if not api_key:
-        st.warning("Por favor, ingresa tu API key.")
+        st.warning("Por favor ingresa tu API key.")
